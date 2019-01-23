2b665ed2c76eb65301076e07f553b91e2b203482
steven <steven@pin2wall.com>
2019-01-23 15:25:54 +0800

    first package

---
+++ src/HelloWorld/Main.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace iisustudio\HelloWorld;
+
+class Main
+{
+    public static function run()
+    {
+        return "Hello World!";
+    }
+}
---
+++ tests/test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+use iisustudio\HelloWorld\Main;
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using
+
+echo Main::run();
+
+
